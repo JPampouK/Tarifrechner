a7fec74b10c3ae9a52e482add5321393bd254176
JPampou <janis@pampoukidis.de>
2022-07-03 08:13:40 +0200

    Tarif Card weiter gemacht

--- src/app/tarif-card/tarif-card.component.ts
+++ src/app/tarif-card/tarif-card.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export class Tarife {
+  constructor(
+  public ID: number,
+  public Bezeichnung: string,
+  public Fuer: string,
+  public Img: string,
+  public Hinweis: string,
+  public Grundpreis: number,
+  public Verbrauchspreis: number
+  ) {}
+}
 
 @Component({
   selector: 'app-tarif-card',
@@ -6,10 +19,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarif-card.component.scss']
 })
 export class TarifCardComponent implements OnInit {
+  Tarife!: Tarife[];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getTarife();
+  }
+
+  getTarife(){
+    this.http.get<any>("http://localhost/tarifrechner/tarife.php").subscribe( respone =>{
+      this.Tarife = respone;
+    })
   }
 
 }
